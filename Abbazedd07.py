37c047d51d5029c3f3d9f290f11a79916929421b
abbazedd07 <81933746+abbazedd07@users.noreply.github.com>
2021-04-04 23:16:23 +0100

    Add files via upload

---
+++ Abbazedd07.py
@@ -0,0 +1,24 @@
+print("NAME:Zakari Zakari Abba")
+print("DEPARTMENT: Computer Science")
+scare = "Boo!"
+print (scare)
+var1 = 56
+var2 = 67
+third_score= var1 + var2
+print(var1, var2, third_score)
+var5 = 12
+var4 = .5
+var6 = var5 / var4
+print (var6)
+one = 34
+two = 56
+three = one + two
+print (three)
+dash = 8
+crash = dash % 3
+print (crash)
+E1 = 2
+E2 = E1 * 4
+B1 = 4
+B2 = B1 + 2
+print (E2 * B1 + 2)
